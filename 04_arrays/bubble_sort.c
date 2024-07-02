31626e00290a534b1281f6c77957bb5b986acedf
kelvinyelyen <kelvinyelyen@gmail.com>
2024-07-02 19:20:05 +0000

    update: new variants

--- 04_arrays/bubble_sort.c
+++ 04_arrays/bubble_sort.c
@@ -2,19 +2,28 @@
 
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = 6;
-    int i, j;
+    int size, i, j;
 
+    printf("Enter the preferred size of array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Enter Array[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+   
     printf("Original array: ");
-    for (i = 0; i < n; i++)
+    for (i = 0; i < size; i++)
         printf("%d ", arr[i]);
 
     printf("\n");
 
-    for (i = 0; i < n ; i++)
+    for (i = 0; i < size ; i++)
     {
-        for (j = 0; j < n - i; j++)
+        for (j = 0; j < size - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
@@ -26,7 +35,7 @@ int main()
     }
 
     printf("Sorted array: ");
-    for (i = 0; i < n; i++)
+    for (i = 0; i < size; i++)
         printf("%d ", arr[i]);
 
     printf("\n");
