86a306e14693f9aa3ed5b6334af44cab1ff60158
kelvinyelyen <kelvinyelyen@gmail.com>
2024-07-12 14:32:19 +0000

    update

--- sandbox1.c
+++ sandbox1.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-int main()
-{
+// int main()
+// {
     // char binary_str[] = "1010";
     // int decimal = strtol(binary_str, NULL, 2);
     // printf("%d\n", decimal);
@@ -39,16 +39,34 @@ int main()
     //     }
     // }
 
-    int num, a, b, c, d, e, x;
-    printf("Enter a five digit number:");
-    scanf("%d", &num);
+//     int num, a, b, c, d, e, x;
+//     printf("Enter a five digit number:");
+//     scanf("%d", &num);
 
-    e = num % 10;
-    d = (num / 10) % 10;
-    c = (num / 100) % 10;
-    b = (num / 1000) % 10;
-    a = (num / 10000) % 10;
+//     e = num % 10;
+//     d = (num / 10) % 10;
+//     c = (num / 100) % 10;
+//     b = (num / 1000) % 10;
+//     a = (num / 10000) % 10;
 
-    x = e*10000 + d*1000 + c*100 + b*10 + a;
-    printf("\n%d", x);
-}
+//     x = e * 10000 + d * 1000 + c * 100 + b * 10 + a;
+//     printf("\n%d", x);
+// }
+
+// #include<stdio.h>
+
+// int add(int a,int b);
+
+// main(){
+//     int num1, num2;
+//     printf("Enter 2 numbers: ");
+//     scanf("%d %d",&num1,&num2);
+
+//     add(num1,num2);
+// }
+
+
+// int add(int a,int b){
+//     int sum = a + b;
+//     printf("The sum of the numbers is %d",sum);
+// }
