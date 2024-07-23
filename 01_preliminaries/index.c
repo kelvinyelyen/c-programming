a8fd86326d8b31ea185866c549d84d9251760666
kelvinyelyen <kelvinyelyen@gmail.com>
2024-07-23 09:09:01 +0000

    update: structuring

---
+++ 01_preliminaries/index.c
@@ -0,0 +1,20 @@
+/*
+ * Basics of C (Syntax, Structure, Variables, Data Types)
+ * Control Structures (if-else, switch, loops)
+ * Functions (Declaration, Definition, Call, Recursion)
+ * Arrays and Strings
+ * Pointers and Dynamic Memory Allocation
+ * Structures and Unions
+ * File I/O
+ * Preprocessor Directives
+ *
+ * Advanced Topics
+ * Dynamic Data Structures (Linked Lists, Stacks, Queues, Trees)
+ * Memory Management and Debugging
+ * C Standard Library Functions (math.h, string.h, stdlib.h, etc.)
+ * Command Line Arguments
+ * Inline Assembly and Low-Level Programming
+ * Multithreading and Concurrency (POSIX threads)
+ * Network Programming (Sockets)
+ * Graphics Programming (using libraries like SDL or OpenGL)
+ */
